c86e50f85a80942b77547f7bfed6406be574fb68
Olena <nikimih@gmail.com>
2023-02-22 22:24:56 +0000

    add Event

---
+++ src/components/Event/Event.jsx
@@ -0,0 +1,28 @@
+export const Event =(name, location, speaker,type, start, end)=>{
+    return (
+        <div>
+            <h2> Name</h2>
+            <p>
+                <i>
+                    Location
+                </i>
+            </p>
+             <p>
+                <i>
+                    Speaker
+                </i>
+            </p>
+            <p>
+                <i>
+                    Date
+                </i>
+            </p>
+            <p>
+                <i>
+                    Duration
+                </i>
+            </p>
+            <span> Event type </span>
+        </div>
+    )
+}
