fa450d172f8a8a3b575bddabccb90876094fb592
Olena <nikimih@gmail.com>
2023-02-22 21:59:52 +0000

    add collection

--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -2,5 +2,8 @@
 import css from './EventBoard.module.css'
 export const EventBoard =({events})=>{
     console.log(events);
-    return <div className={css.eventBoard}>Event cards</div>
+    return <div className={css.eventBoard}>
+        {events.map(event=>{
+            <div key={event.name}>{event.name}</div>
+        })}</div>
 }
