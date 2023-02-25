cb34c5c95838c1e55dcd845a9ba463dd4b0106dd
Olena <nikimih@gmail.com>
2023-02-25 21:06:09 +0000

    add event styles

--- src/components/Event/Event.jsx
+++ src/components/Event/Event.jsx
@@ -1,23 +1,27 @@
-export const Event =(name, location, speaker,type, start, end)=>{
+import PropTypes from 'prop-types'
+import css from './Event.module.css'
+export const Event =({name, location, speaker,type, start, end})=>{
+    console.log(css)
     return (
-        <div>
-            <h2> Name</h2>
-            <p>
-                <i>
+        
+        <div className={css.event}>
+            <h2 className={css.title}> Name</h2>
+            <p className={css.info}>
+                <i className='icon'>
                     Location
                 </i>
             </p>
-             <p>
+             <p className={css.info}>
                 <i>
                     Speaker
                 </i>
             </p>
-            <p>
+            <p className={css.info}>
                 <i>
                     Date
                 </i>
             </p>
-            <p>
+            <p className={css.info}>
                 <i>
                     Duration
                 </i>
@@ -26,3 +30,12 @@ export const Event =(name, location, speaker,type, start, end)=>{
         </div>
     )
 }
+Event.propTypes={
+    name: PropTypes.string.isRequired, 
+    location: PropTypes.string.isRequired, 
+    speaker:PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired, 
+    start:PropTypes.string.isRequired, 
+    end:PropTypes.string.isRequired,
+}
+    
--- src/components/EventBoard/EventBoard.jsx
+++ src/components/EventBoard/EventBoard.jsx
@@ -1,10 +1,31 @@
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
+import {Event} from 'components/Event/Event'
 import css from './EventBoard.module.css'
 export const EventBoard =({events})=>{
         return (<div className={css.eventBoard}>
-            {events.map (event=>(
-    <div key={event.name}>{event.name}</div>
+            {events.map (({name,location,speaker,type,time})=>(
+    <Event key={name}
+    name={name}
+    location={location}
+    speaker={speaker}
+    type={type}
+    start={time.start}
+    end={time.end}
+    />
 ))}
         </div>)
 }
 
+EventBoard.propTypes={
+    events:PropTypes.arrayOf(
+        PropTypes.exact({
+    name: PropTypes.string.isRequired, 
+    location: PropTypes.string.isRequired, 
+    speaker:PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired, 
+    time:{
+        start:PropTypes.string.isRequired, 
+        end:PropTypes.string.isRequired,}
+        })
+    )
+}
