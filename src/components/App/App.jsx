e0664a681914613e728af743bbb4ca11bc62d38a
Olena <nikimih@gmail.com>
2023-03-02 09:09:14 +0000

    add emotion/understand idea

---
+++ src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import { EventBoard } from "../EventBoard/EventBoard";
+import { PageTitle } from "../PageTitle/PageTitle";
+import upcomingEvents from 'upcoming-events';
+import { Container } from "./App.styled";
+
+export const App = () => {
+  return (
+    <Container>
+   <PageTitle text="24th Core Worlds Coalition Conference"/>
+    <EventBoard events={upcomingEvents}/>
+    </Container>) ;
+};
